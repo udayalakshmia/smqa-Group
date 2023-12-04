f2138e167daf88d587ad9fe9b6e6dad10de5033f
Udaya Lakshmi Ankathi <ula1@student.le.ac.uk>
2023-12-04 19:03:32 +0000

    Clean up the solution and added files to set up the back end changes

---
+++ mcqa/src/test/java/com/mcqa/McqaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McqaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
