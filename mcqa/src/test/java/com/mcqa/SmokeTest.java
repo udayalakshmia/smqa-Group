ffde9fa9ac86279483181cd2dc1e8f8881c4341f
Udaya Lakshmi Ankathi <ula1@student.le.ac.uk>
2023-12-09 16:46:57 +0000

    Added the Junit test cases. Added White box and Black Box testing. Improved the test Coverage to 70%

---
+++ mcqa/src/test/java/com/mcqa/SmokeTest.java
@@ -0,0 +1,28 @@
+package com.mcqa;
+
+
+import com.mcqa.controller.AdminController;
+import com.mcqa.controller.EducatorController;
+import com.mcqa.controller.LoginController;
+import com.mcqa.controller.StudentController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class SmokeTest {
+
+
+
+    @Autowired
+    private LoginController loginController;
+    @Autowired
+    private AdminController adminController;
+
+    @Test
+    void contextLoads() throws Exception {
+        assertThat(loginController).isNotNull();
+        assertThat(adminController).isNotNull();
+    }
+}
